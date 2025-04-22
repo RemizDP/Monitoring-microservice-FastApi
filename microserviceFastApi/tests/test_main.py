ce18d793405acc44b87ba5d41e6eace8c36838ae
RemizDP <135232733+RemizDP@users.noreply.github.com>
2025-04-22 23:01:13 +0300

    Update test_main.py

--- microserviceFastApi/tests/test_main.py
+++ microserviceFastApi/tests/test_main.py
@@ -1,12 +1,12 @@
 import pytest
 import datetime
 import os, sys
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from ..app.main import app, Base, get_db, Event, User
+from main import app, Base, get_db, Event, User
 
 # Задайте строку подключения к тестовой базе PostgreSQL
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:0000@localhost:5432/LoggingMicroservice"
